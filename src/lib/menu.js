7077fac7cd172cafd35d07267b52d58fa11fa60e
Gonzalo <gonzalorom2001@gmail.com>
2026-08-04 13:10:12 -0300

    sin foto

--- src/lib/menu.js
+++ src/lib/menu.js
@@ -5,7 +5,7 @@ export const MENU = [
     nombre: 'Burger Nahitan',
     ingredientes: ['Triple cheddar Milkau', 'Medallón 120gr', 'Panceta ahumada', 'Cebolla caramelizada', 'Mayo Nahitan'],
     incluye: 'Con papas McCain y dip de mayo.',
-    imagen: '/nahitan/nahitan.png',
+    imagen: '/sin-imagen.png',
     precios: { Simple: 13000, Doble: 15000, Triple: 17000 },
   },
   {
@@ -13,7 +13,7 @@ export const MENU = [
     nombre: 'Burger Cheese',
     ingredientes: ['Cuádruple cheddar Milkau', 'Medallón 120gr', 'Panceta ahumada', 'Barbacoa', 'Mayo Nahitan'],
     incluye: 'Con papas McCain y dip de mayo.',
-    imagen: '/cheese/cheese.webp',
+    imagen: '/sin-imagen.png',
     precios: { Simple: 12500, Doble: 14500, Triple: 16500 },
   },
   {
@@ -21,7 +21,7 @@ export const MENU = [
     nombre: 'Burger Cheese Onion',
     ingredientes: ['Doble cheddar Milkau', 'Medallón 120gr', 'Panceta ahumada', '3 aros de cebolla rebozados', 'Mayo Nahitan'],
     incluye: 'Con papas McCain y dip de mayo.',
-    imagen: '/cheese-onion/cheese-onion.webp',
+    imagen: '/sin-imagen.png',
     precios: { Simple: 13150, Doble: 15150, Triple: 17150 },
   },
   {
@@ -29,7 +29,7 @@ export const MENU = [
     nombre: 'Burger Especial',
     ingredientes: ['Triple queso Dambo La Paulina', 'Medallón 120gr', 'Jamón cocido Lario', 'Lechuga fresca', 'Tomate', 'Huevo', 'Mayo Nahitan'],
     incluye: 'Con papas McCain y dip de mayo.',
-    imagen: '/especial/especial.png',
+    imagen: '/sin-imagen.png',
     precios: { Simple: 13000, Doble: 15000, Triple: 17000 },
   },
   {
@@ -37,7 +37,7 @@ export const MENU = [
     nombre: 'Burger De Lujo',
     ingredientes: ['Triple cheddar Milkau', 'Medallón 120gr', 'Panceta ahumada', 'Cebolla morada', 'Lechuga fresca', 'Tomate', 'Mayo Nahitan'],
     incluye: 'Con papas McCain y dip de mayo.',
-    imagen: '/de-lujo/de-lujo.png',
+    imagen: '/sin-imagen.png',
     precios: { Simple: 14000, Doble: 16000, Triple: 18000 },
   },
   {
@@ -45,7 +45,7 @@ export const MENU = [
     nombre: 'Burger Oklahoma',
     ingredientes: ['Triple cheddar Milkau', 'Medallón smasheado con cebolla', 'Mayo Nahitan'],
     incluye: 'Con papas McCain y dip de mayo.',
-    imagen: '/oklahoma/oklahoma.png',
+    imagen: '/sin-imagen.png',
     precios: { Simple: 12000, Doble: 14000, Triple: 16000 },
   },
   {
@@ -53,7 +53,7 @@ export const MENU = [
     nombre: 'Burger Roque',
     ingredientes: ['Medallón 120gr (uno smasheado c/ cebolla y roquefort, otro con Dambo)', 'Panceta ahumada', 'Mayo Nahitan'],
     incluye: 'Con papas McCain y dip de mayo.',
-    imagen: '/roque/roque.png',
+    imagen: '/sin-imagen.png',
     precios: { Simple: 15000, Doble: 17000, Triple: 19000 },
   },
   {
@@ -61,7 +61,7 @@ export const MENU = [
     nombre: 'Burger Argentum',
     ingredientes: ['Medallón 120gr', 'Provoleta', 'Morrones asados', 'Mayo Nahitan'],
     incluye: 'Con papas McCain y dip de mayo.',
-    imagen: '/aegentum/aegentum.png',
+    imagen: '/sin-imagen.png',
     precios: { Simple: 13000, Doble: 15500, Triple: 17500 },
   },
 ];
